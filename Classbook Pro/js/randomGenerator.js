61311ccd2e484bc5135f91b2685e7af9bb39c6d1
admin <admin@DESKTOP-V9JPPPN.fritz.box>
2019-09-20 18:52:39 +0200

    first commit

---
+++ Classbook Pro/js/randomGenerator.js
@@ -0,0 +1,23 @@
+function load() {
+  window.alert("test");
+}
+
+function checkRandom() {
+  var selectedRandom = document.getElementById("select").value;
+  if(selectedRandom == "schueler") {
+    console.log("Schüler-Modus");
+  } else if(selectedRandom == "numbers") {
+    console.log("Nummer-Modus");
+    setNumbers();
+  } else if(selectedRandom == "letters") {
+    console.log("Alphabet-Modus");
+  } else {
+    console.log("Undefinierte Eingabe!");
+  }
+}
+
+function setNumbers() {
+  var randomDiv = document.getElementById("random");
+  //To Do Auswahlfelder adden
+}
+//Test
